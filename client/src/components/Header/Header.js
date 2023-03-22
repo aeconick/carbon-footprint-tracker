461c46c82d711f1ebb69b89c6218b928101a6422
Nikolay Cherepov <114342845+aeconick@users.noreply.github.com>
2023-03-22 22:59:36 +0200

    Add Header component

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,20 @@
+export const Header = () => {
+    return (
+        <header>
+            <h1><a class="home" href="#">CarbonTracker</a></h1>
+            <nav>
+                <a href="#">All logs</a>
+                {/* Logged */}
+                <div id="user">
+                    <a href="#">Log Emission</a>
+                    <a href="#">Logout</a>
+                </div>
+                {/* Guest */}
+                <div id="guest">
+                    <a href="#">Login</a>
+                    <a href="#">Register</a>
+                </div>
+            </nav>
+        </header>
+    );
+};
