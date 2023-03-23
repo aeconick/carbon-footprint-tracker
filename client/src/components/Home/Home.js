41570daac45bfbb32b1772f8c599b55c1295df1e
Nikolay Cherepov <114342845+aeconick@users.noreply.github.com>
2023-03-23 20:37:17 +0200

    Update Home page style

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -8,55 +8,50 @@ export const Home = () => {
                 <h2>Track your emissions</h2>
                 <h3>Keep global warming away</h3>
             </div>
-            <img src="/images/planet.jpg" alt="planet" />
+            <img src="/images/planet.png" alt="planet" />
 
             <div id="home-page">
-                <h1>Reviews from famous climate activists:</h1>
+                <h1>Talks from famous climate activists:</h1>
 
                 <div className="log">
                     <div className="image-wrap">
                         <img src="/images/greta.jpg" />
                     </div>
                     <h3>Greta Thunberg</h3>
-
+                    <div class="quotes">
+                        <span>lqlqlql</span>
+                    </div>
                     <div className="data-buttons">
-                        <a href="#" className="btn details-btn">Review</a>
+                        <a href="#" className="btn details-btn">Read More</a>
                     </div>
                 </div>
 
                 <div className="log">
                     <div className="image-wrap">
-                        <img src="" />
+                        <img src="/images/leonardo.jpg" />
                     </div>
-                    <h3>Dummie name</h3>
-
+                    <h3>Leonardo Dicaprio</h3>
+                    <div class="quotes">
+                        <span>lqlqlqlq</span>
+                    </div>
                     <div className="data-buttons">
-                        <a href="#" className="btn details-btn">Review</a>
+                        <a href="#" className="btn details-btn">Read More</a>
                     </div>
                 </div>
 
                 <div className="log">
                     <div className="image-wrap">
-                        <img src="" />
+                        <img src="/images/shailene.jpg" />
                     </div>
-                    <h3>Dummie name</h3>
-                    
+                    <h3>Shailene Woodley</h3>
+                    <div class="quotes">
+                        <span>lqlqlq</span>
+                    </div>
                     <div className="data-buttons">
-                        <a href="#" className="btn details-btn">Review</a>
+                        <a href="#" className="btn details-btn">Read More</a>
                     </div>
                 </div>
 
-                <div className="log">
-                    <div className="image-wrap">
-                        <img src="" />
-                    </div>
-                    <h3>Dummie name</h3>
-                    
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">Review</a>
-                    </div>
-                </div>
-                
             </div>
         </section>
     );
