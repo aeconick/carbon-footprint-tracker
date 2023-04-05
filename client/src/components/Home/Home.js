0caede19f955fdd3fc6d30bc8b48eab75e6aa019
Nikolay Cherepov <114342845+aeconick@users.noreply.github.com>
2023-04-05 22:48:22 +0300

    Update home page

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,58 +1,24 @@
+import { Link } from 'react-router-dom';
+
 import './Home.css';
 
 export const Home = () => {
     return (
         <section id="welcome-world">
-
             <div className="welcome-message">
                 <h2>Track your emissions</h2>
                 <h3>Keep global warming away</h3>
+                <h4>
+                    <Link to={'/register'}>
+                        Register
+                    </Link>
+                    to start tracking or learn
+                    <Link to={'/about'}>
+                        more!
+                    </Link>
+                </h4>
             </div>
             <img src="/images/planet.png" alt="planet" />
-
-            <div id="home-page">
-                <h1>Talks from famous climate activists:</h1>
-
-                <div className="log">
-                    <div className="image-wrap">
-                        <img src="/images/greta.jpg" />
-                    </div>
-                    <h3>Greta Thunberg</h3>
-                    <div className="quotes">
-                        <span>lqlqlql</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">Read More</a>
-                    </div>
-                </div>
-
-                <div className="log">
-                    <div className="image-wrap">
-                        <img src="/images/leonardo.jpg" />
-                    </div>
-                    <h3>Leonardo Dicaprio</h3>
-                    <div className="quotes">
-                        <span>lqlqlqlq</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">Read More</a>
-                    </div>
-                </div>
-
-                <div className="log">
-                    <div className="image-wrap">
-                        <img src="/images/shailene.jpg" />
-                    </div>
-                    <h3>Shailene Woodley</h3>
-                    <div className="quotes">
-                        <span>lqlqlq</span>
-                    </div>
-                    <div className="data-buttons">
-                        <a href="#" className="btn details-btn">Read More</a>
-                    </div>
-                </div>
-
-            </div>
         </section>
     );
 };
